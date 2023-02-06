16a47afa0242c318bed14cd8318895b97302a68d
Meysam-Arshadi <meysamarshadi@gmail.com>
2023-02-06 19:42:04 +0000

    Dog Photo Done

---
+++ challenges/dog-photo-gallery/script.js
@@ -0,0 +1,24 @@
+const button = document.createElement("button");
+button.innerHTML = "Show me a dog!";
+document.body.appendChild(button);
+
+let element = document.createElement("ul");
+document.body.appendChild(element);
+
+button.addEventListener("click", getAPicture);
+
+let list = document.createElement("li");
+let image = document.createElement("img");
+
+async function getAPicture() {
+  try {
+    let response = await fetch("https://dog.ceo/api/breeds/image/random");
+    let dogURL = await response.json();
+    console.log(dogURL);
+    image.src = dogURL.message;
+    list.appendChild(image);
+    element.appendChild(list);
+  } catch (err) {
+    console.log("Error");
+  }
+}
